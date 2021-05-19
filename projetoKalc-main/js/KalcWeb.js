3a137c5f0cbb28344eaa223fefcf7ddaf036beb8
vandeilltton <vandeilltton@gmail.com>
2021-05-19 03:19:52 -0300

    projetoKalc

---
+++ projetoKalc-main/js/KalcWeb.js
@@ -0,0 +1,97 @@
+console.log("Teste  calculadora");
+
+// Dom 
+ 			
+//Operações
+const div = document.getElementById("div");
+const mult = document.getElementById("mult");
+const subtracao = document.getElementById("subtracao");
+const soma = document.getElementById("soma");
+const igual = document.getElementById("igual");
+
+// Numeros 
+const zero = document.getElementById("zero");
+const um = document.getElementById("um");
+const dois = document.getElementById("dois");
+const tres = document.getElementById("tres");
+const quatro = document.getElementById("quatro");
+const cinco = document.getElementById("cinco");
+const seis = document.getElementById("seis");
+const sete = document.getElementById("sete");
+const oito = document.getElementById("oito");
+const nove = document.getElementById("nove");
+const ponto = document.getElementById("ponto");
+const limpa = document.getElementById("limpa");
+
+// Eventos
+
+//Operações
+div.onclick = function () {
+	console.log(div.innerText);
+};
+
+mult.onclick = function () {
+	console.log(mult.innerText);
+};
+
+subtracao.onclick = function () {
+	console.log(subtracao.innerText);
+};
+
+soma.onclick = function () {
+	console.log(soma.innerText);
+};
+
+igual.onclick = function() {
+	console.log(igual.innerText);
+};
+
+//Numeros
+
+zero.onclick = function () {
+	console.log(zero.innerText);
+};
+
+um.onclick = function () {
+	console.log(um.innerText);
+};
+
+dois.onclick = function () {
+	console.log(dois.innerText);
+};
+
+tres.onclick = function () {
+	console.log(tres.innerText);
+};
+
+quatro.onclick = function() {
+	console.log(quatro.innerText);
+};
+
+cinco.onclick = function () {
+	console.log(cinco.innerText);
+};
+
+seis.onclick = function () {
+	console.log(seis.innerText);
+};
+
+sete.onclick = function () {
+	console.log(sete.innerText);
+};
+
+oito.onclick = function () {
+	console.log(oito.innerText);
+};
+
+nove.onclick = function () {
+	console.log(nove.innerText);
+};
+
+ponto.onclick = function () {
+	console.log(ponto.innerText);
+};
+
+limpa.onclick = function () {
+	console.log(limpa.innerText);
+};
